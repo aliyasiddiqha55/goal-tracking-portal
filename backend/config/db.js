f85734d0bdced6fe665ec753d12c3cac9db47ceb
Aliya Siddiqha <aliyaSiddiqha977@gmail.com>
2026-05-17 15:41:27 +0530

    Force IPv4 database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  family: 4
 });
 
 module.exports = pool;
